041564c57179b3cfef7e4206a496ef33657161e6
Max Zernyaev <winterzz@internet.ru>
2024-09-27 23:31:03 +0300

    feat: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+const { configure, presets } = require('eslint-kit')
+
+module.exports = configure({
+  presets: [
+    presets.imports({
+      sort: {
+        newline: false,
+      },
+    }),
+    presets.typescript(),
+    presets.prettier(),
+    presets.node(),
+  ],
+})
